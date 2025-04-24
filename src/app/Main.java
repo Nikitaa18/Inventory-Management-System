c983d00eb7c01599327fccbf921d19f7339229d3
Nikita <155805766+Nikitaa18@users.noreply.github.com>
2025-04-24 11:09:21 +0530

    Initial commit of inventory management system

---
+++ src/app/Main.java
@@ -0,0 +1,8 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Inventory Management System Started");
+    }
+}
+// This is the main entry point of the application. It initializes the system and starts the inventory management process.
